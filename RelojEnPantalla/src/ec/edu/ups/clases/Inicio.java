f5e2d51f9a61b253aa9142c6744e0e488a4e864d
adrianmj <adrianmj@ARMJ>
2017-07-18 16:21:45 -0500

    identacion

--- RelojEnPantalla/src/ec/edu/ups/clases/Inicio.java
+++ RelojEnPantalla/src/ec/edu/ups/clases/Inicio.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ec.edu.ups.clases;
 
 /**
@@ -11,13 +10,14 @@ package ec.edu.ups.clases;
  * @author Ing-Software
  */
 public class Inicio {
+
     public static void main(String[] args) {
-    Reloj mihilo = new Reloj("Reloj en pantalla");//Nombre del proyecto
+        Reloj mihilo = new Reloj("Reloj en pantalla");//Nombre del proyecto
         System.out.println("Version 1.0"); //etiqueta del software
-    mihilo.start(); // empieza el reloj en pantalla
+        mihilo.start(); // empieza el reloj en pantalla
 //        OpcionalReloj or = new OpcionalReloj();
 //        or.reloj();
-        
+
     }
-    
+
 }
